97b17c0c45099f9a21e7d2a35131873dc8e390c1
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-09-27 19:25:58 -0300

    Commit Initial

---
+++ lib/views/product_overview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../models/product.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> leadedProducts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
