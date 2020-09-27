a229907d5e062426b7e1bde8066d1f0a013605bd
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-09-27 19:45:38 -0300

    Gridview builder

--- lib/views/product_overview_screen.dart
+++ lib/views/product_overview_screen.dart
@@ -3,10 +3,25 @@ import '../data/dummy_data.dart';
 import '../models/product.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-  final List<Product> leadedProducts = DUMMY_PRODUCTS;
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
   }
 }
