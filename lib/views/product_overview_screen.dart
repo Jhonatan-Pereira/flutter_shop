df1e4e1931b155ab4af4022b341daa8476a95567
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-06 20:29:16 -0300

    Create Widget Product Grid

--- lib/views/product_overview_screen.dart
+++ lib/views/product_overview_screen.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../widgets/product_item.dart';
-import '../providers/products.dart';
+import '../widgets/product_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (ctx, i) => ProductItem(products[i]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/product_item.dart';
+import '../providers/products.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(products[i]),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
