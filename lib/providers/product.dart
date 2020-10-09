4c6a6d032ff477508713b1ad539dfa80c9d9f2ea
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-09 10:12:27 -0300

    change favorite products from firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,27 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  void _toggleFavorite() {
     isFavorite = !isFavorite;
     notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    _toggleFavorite();
+
+    try {
+      final response = await http.patch(
+        'https://flutter-shop-57aa7.firebaseio.com/products/$id.json',
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+      }
+    } catch (e) {
+      _toggleFavorite();
+    }
   }
 }
