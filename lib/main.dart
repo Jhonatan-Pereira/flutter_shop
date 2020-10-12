395e8a71d398cd738bbbdecedc8ac0337c93f2b4
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-12 15:50:57 -0300

    associate order to user

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class MyApp extends StatelessWidget {
           create: (_) => Orders(),
           update: (ctx, auth, previousOrders) => Orders(
             auth.token,
+            auth.userId,
             previousOrders.items,
           ),
         ),
--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -17,10 +17,11 @@ class Order {
 
 class Orders with ChangeNotifier {
   String _token;
+  String _userId;
   List<Order> _items = [];
   final _baseUrl = '${Constants.BASE_API_URL}/orders';
 
-  Orders([this._token, this._items = const []]);
+  Orders([this._token, this._userId, this._items = const []]);
 
   List<Order> get items {
     return [..._items];
@@ -32,7 +33,7 @@ class Orders with ChangeNotifier {
 
   Future<void> loadOrders() async {
     List<Order> loadedItems = [];
-    final response = await http.get("$_baseUrl.json?auth=$_token");
+    final response = await http.get("$_baseUrl/$_userId.json?auth=$_token");
     print(json.decode(response.body));
     Map<String, dynamic> data = json.decode(response.body);
 
@@ -69,7 +70,7 @@ class Orders with ChangeNotifier {
     final date = DateTime.now();
 
     final response = await http.post(
-      "$_baseUrl.json?auth=$_token",
+      "$_baseUrl/$_userId.json?auth=$_token",
       body: json.encode({
         'amount': cart.totalAmount,
         'date': date.toIso8601String(),
