e73aff4ede46bac976429610b2db10c8c41a3594
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-13 16:13:32 -0300

    hero transition

--- lib/widgets/product_grid_item.dart
+++ lib/widgets/product_grid_item.dart
@@ -22,10 +22,13 @@ class ProductGridItem extends StatelessWidget {
               arguments: product,
             );
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/productplaceholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/productplaceholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
