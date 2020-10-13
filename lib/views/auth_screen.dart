eddabbeeb24b2afb8c9586cc2aefeacc2b0e91da
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-13 15:43:38 -0300

    responsiveness correction

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -21,41 +21,47 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  padding: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 70,
-                  ),
-                  transform: Matrix4.rotationZ(-8 * (pi / 180))
-                    ..translate(-10.0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.deepOrange.shade900,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, 2),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 45),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 70,
+                      ),
+                      transform: Matrix4.rotationZ(-8 * (pi / 180))
+                        ..translate(-10.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrange.shade900,
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                            ),
+                          ]),
+                      child: Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                          color:
+                              Theme.of(context).accentTextTheme.headline6.color,
+                          fontSize: 45,
+                          fontFamily: 'Anton',
                         ),
-                      ]),
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                      color: Theme.of(context).accentTextTheme.headline6.color,
-                      fontSize: 45,
-                      fontFamily: 'Anton',
+                      ),
                     ),
-                  ),
+                    AuthCard(),
+                  ],
                 ),
-                AuthCard(),
-              ],
+              ),
             ),
           ),
         ],
