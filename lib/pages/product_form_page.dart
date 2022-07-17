aa91f965e81495ca2f71b9076fa98305eb1686c4
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-07-17 20:21:49 -0300

    feat: handle request response error

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -82,7 +82,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
     Provider.of<ProductList>(
       context,
       listen: false,
-    ).saveProduct(_formData).then((value) {
+    ).saveProduct(_formData).catchError((error) {
+      return showDialog<void>(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Ocorreu um erro!'),
+          content: const Text('Erro para salvar produto'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Ok'),
+            ),
+          ],
+        ),
+      );
+    }).then((value) {
       setState(() => _isLoading = false);
       Navigator.of(context).pop();
     });
