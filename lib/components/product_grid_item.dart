a6a8ff72bf062192ee1826983246738dfb704329
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-09-20 15:59:07 -0300

    feat: implement transition hero

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -67,11 +67,14 @@ class ProductGridItem extends StatelessWidget {
           ),
         ),
         child: GestureDetector(
-          child: FadeInImage(
-            placeholder:
-                const AssetImage('assets/images/productplaceholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder:
+                  const AssetImage('assets/images/productplaceholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
           onTap: () {
             Navigator.of(context).pushNamed(
