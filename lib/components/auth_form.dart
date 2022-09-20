1e2dac85b1cc43fe01c2cfe7e605e204af2037d0
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-09-20 15:28:16 -0300

    feat: implement animated builder

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -48,8 +48,6 @@ class _AuthFormState extends State<AuthForm>
         curve: Curves.linear,
       ),
     );
-
-    _heightAnimation?.addListener(() => setState(() {}));
   }
 
   @override
@@ -127,11 +125,14 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        // height: _isLogin() ? 310 : 400,
-        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-        width: deviceSize.width * 0.75,
+      child: AnimatedBuilder(
+        animation: _heightAnimation!,
+        builder: (ctx, childForm) => Container(
+          padding: const EdgeInsets.all(16),
+          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+          width: deviceSize.width * 0.75,
+          child: childForm,
+        ),
         child: Form(
           key: _formKey,
           child: Column(
